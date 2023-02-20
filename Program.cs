12f4829263f03498d9ef40360364301abf8f43d2
kachmartaras <kachmartaras99@gmail.com>
2023-02-20 15:12:35 +0200

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var fibonachiNumbers = new List<int> {1 , 1};
+
+        for (int i = 0; i < 20; i++)
+        {
+            var previous = fibonachiNumbers[fibonachiNumbers.Count - 1];
+            var previous2 = fibonachiNumbers[fibonachiNumbers.Count - 2];
+
+            fibonachiNumbers.Add(previous + previous2);
+        }
+
+        foreach (var item in fibonachiNumbers)
+        {
+            Console.WriteLine(item);
+        }
+
+
+    }
+}
